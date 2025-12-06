22b80e231d45ca37c5096d843016fde94d9bddb7
Brxksd <brenogbragavieira@gmail.com>
2025-12-06 12:51:36 -0300

    Update Exercise 17 | Python

--- Curso em vídeo/Python/Exerícios/Módulos/desafio17.py
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio17.py
@@ -5,4 +5,4 @@ co = float(input('Digite o comprimento do cateto oposto\n-> '))
 
 hip = math.sqrt((math.pow(ca,2) + math.pow(co,2)))
 
-print('A hipotenusa tem comprimento de {}.'.format(hip))
+print('A hipotenusa tem comprimento de {:.2f}.'.format(hip))
