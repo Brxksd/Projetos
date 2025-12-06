c3823ffa22b40ad12cae04ce70d68cbeb8ba7656
Brxksd <brenogbragavieira@gmail.com>
2025-12-06 19:03:59 -0300

    Added Exercise 22 | Python

---
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio22.py
@@ -0,0 +1,6 @@
+frase = input('Digite uma frase\n-> ')
+
+frasenew = frase.split()
+frasejunta = ''.join(frasenew)
+
+print('{}\n{}\n{}\n{}'.format(frase.upper(),frase.lower(),len(frasejunta),len(frasenew [0]), ))
