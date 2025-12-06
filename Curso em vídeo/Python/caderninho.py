22ffa63effcda6edce7482a661cb3f63e67c121d
Brxksd <brenogbragavieira@gmail.com>
2025-12-06 19:03:21 -0300

    Update caderninho.py

--- Curso em vídeo/Python/caderninho.py
+++ Curso em vídeo/Python/caderninho.py
@@ -1,7 +1,7 @@
 # ==============================================================
 
 #  Aqui, deixarei os meus rascunhos de códigos e testes que podem
-# ser rodados quando retirados os '#' entre as "páginas".
+#  ser rodados quando retirados os '#' entre as "páginas".
 #  Quando tem '##', é uma anotação.
 
 # ==============================================================
@@ -18,7 +18,7 @@
 
 ## sqrt | Calcular a raiz quadrada de um número
 ## hypot | Calcular a hipotenusa de um triangulo, levando como
-##   parâmetros o cateto oposto e o cateto adjacente
+##    parâmetros o cateto oposto e o cateto adjacente
 
 ## ceil | Arredondar um número para cima
 ## floor | Arredondar um número para baixo
@@ -27,7 +27,7 @@
 ## factorial | Calcular o fatorial de um número
 
 ## trunc | "Truncar" um número, eliminando tudo que tiver da 
-##   vírgula pra frente, ou seja, vai mostrar a parte inteira
+##    vírgula pra frente, ou seja, vai mostrar a parte inteira
 
 # --------------------------------------------------------------
 
@@ -38,6 +38,54 @@
 
 ## choice | Escolhe um elemento aleatório de uma lista
 ## shuffle | Mistura os elementos de uma lista
+
+# ==============================================================
+
+## STRINGS
+
+# frase[9] | Vai retornar o caractere da string "frase" na 
+#    posição 9
+# frase[9:13] | Vai retornar os caracteres da string "frase"
+#    entre a posição 9 até 12 (subtrai 1 do limite final)
+# frase[9:21:2] | Vai retornar os caracteres da string 
+#    "frase" da posição 9 até 20 pulando de 2 em 2
+# frase[:5] | Vai retornar os caracteres da string "frase" da 
+#    posição 0 até 4
+# frase[8:] | Vai retornar os caracteres da string "frase" da 
+#    posição 8 até o final
+# frase[9::3] | Vai retornar os caracteres da string "frase" 
+#    da posição 9 até o final pulando de 3 em 3
+# frase.count('o') | Vai contar quantas vezes aparece a letra 
+#    "o" na string "frase"
+# frase.count('o',0,13) | Vai contar quantas vezes aparece a 
+#    letra "o" da posição 0 até 12 da string "frase"
+# frase.find('deo') | Vai encontrar a posição na string que 
+#    começa a frase "deo"
+# frase.replace('Python','Android') | Vai substituir por Android 
+#    onde ocorre "Python" na string "frase"
+# frase.upper() | O que não estiver em maiúsculo na string "frase" 
+#    vai ficar maiúsculo
+# frase.lower() | O que não estiver em minúsculo na string "frase" 
+#    vai ficar minúsculo
+# frase.capitalize() | Vai pegar toda a string "frase" e deixa-la
+#    minúscula, para colocar a primeira letra da string toda em 
+#    maiúscula
+# frase.title() | Vai pegar toda a string "frase" e deixa-la
+#    minúscula, para colocar a primeira letra de cada palavra em
+#    maiúscula
+# frase.strip() | Vai excluir os espaços vazios no final da string 
+#    "frase"
+# frase.rstrip() | Vai excluir os espaços vazios no começo da string 
+#    "frase"
+# frase.lstrip() | Vai excluir os espaços vazios no começo e final 
+#    da string "frase"
+# frase.split() | Vai quebrar uma string em seus espaços vazios e 
+#    criará uma lista com as palavras que estavam dentro da string
+# 'Curso' in frase | Vai verificar se existe "Curso" na string 
+#    "frase", retornando TRUE ou FALSE
+# len(frase) | Retorna o comprimento da string "frase"
+# '-'.join(frase) | Vai criar uma string "frase" contendo a junção 
+#    das palavras presentes na lista e separa-las por "-" 
 
 # ==============================================================
 
