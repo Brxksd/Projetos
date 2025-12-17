b77e8e18d226e3750e164c6fc4cb6628c1fc33fb
Brxksd <brenogbragavieira@gmail.com>
2025-12-17 20:04:50 -0300

    Update Exercise 22 | Python

--- Curso em vídeo/Python/Exerícios/Módulos/desafio22.py
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio22.py
@@ -3,4 +3,4 @@ frase = str(input('Digite uma frase\n-> '))
 frasenew = frase.split()
 frasejunta = ''.join(frasenew)
 
-print('{}\n{}\n{}\n{}'.format(frase.upper(),frase.lower(),len(frasejunta),len(frasenew [0]), ))
+print('{}\n{}\n{}\n{}'.format(frase.upper(),frase.lower(),len(frasejunta),len(frasenew [0])))
