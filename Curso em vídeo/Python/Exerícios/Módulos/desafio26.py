55a54d0672b938943be7662b24daf8b5bf855a2d
Brxksd <brenogbragavieira@gmail.com>
2025-12-17 19:36:15 -0300

    Added Exercise 26 | Python

---
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio26.py
@@ -0,0 +1,7 @@
+frase = input('Digite uma frase:\n-> ')
+
+num = frase.count('a') + frase.count('A')
+pri = frase.lower().find('a')
+ul = frase.lower().rfind('a')
+
+print('\nA letra "a" aparece um total de {} vezes.\nSua primeira posição é {} e sua última é {}.\n'.format(num,pri,ul))
