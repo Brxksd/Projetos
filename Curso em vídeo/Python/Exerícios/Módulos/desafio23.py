118f230a1947e677c75b59b8d3728bd434200058
Brxksd <brenogbragavieira@gmail.com>
2025-12-17 20:12:07 -0300

    Update Exercise 23 | Python

--- Curso em vídeo/Python/Exerícios/Módulos/desafio23.py
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio23.py
@@ -1,8 +1,8 @@
-num = str(input('Digite um número entre 0 e 9999 (use o formato XXXX):\n-> '))
+num = int(input('Digite um número entre 0 e 9999 (use o formato XXXX):\n-> '))
 
-m = num[0]
-c = num[1]
-d = num[2]
-u = num[3]
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 
 print('\nUnidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}\n'.format(u,d,c,m))
