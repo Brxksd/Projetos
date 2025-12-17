dd4ce06b934d244344a9986c4a8b81b50f64de33
Brxksd <brenogbragavieira@gmail.com>
2025-12-17 19:13:28 -0300

    Added Exercise 24 | Python

---
+++ Curso em vídeo/Python/Exerícios/Módulos/desafio24.py
@@ -0,0 +1,6 @@
+cidade = input('Digite o nome de uma cidade:\n-> ')
+
+if cidade.find('Santo') == 0 :
+    print('\nO nome da cidade começa com Santo.\n')
+else :
+    print('\nO nome da cidade não começa com Santo.\n')
